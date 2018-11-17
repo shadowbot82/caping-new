3c6cf22ad20ba90369422eec5232cdc90b5aca59
shadowbot82 <42908347+shadowbot82@users.noreply.github.com>
2018-11-17 04:39:30 +0000

    Update caping.php

--- caping.php
+++ caping.php
@@ -150,26 +150,7 @@ $klik1 = json_decode($klik,true);
 	    }
 endwhile;
 sleep(1);
-//bonus
-sleep(1);
-echo $t.$turkis."[*] ".$putih."membuka bonus >1<". $t;
-sleep(1)
-	$b=0;
-while ($b < 5):
-$b++;
-flush();
-$bonus		=	($uid,$n);
-$bonus1 = json_decode($bonus,true);
-	if ($bonus1['message'] == 'OK') {
-	    echo $ijo."    ==> BERHASIL ";
-	    }
-	    else
-	    {
-	    $red."[!] => GAGAL !";
-	    }
-endwhile;
-sleep(1);
-	
+
 //result
 $ch = curl_init();
 			curl_setopt($ch, CURLOPT_URL, "https://ai.caping.co.id/v2/user/ccsp/detail"); 
@@ -258,27 +239,6 @@ function klik($uid,$n){
 		$result = curl_exec($ch);
 	return $result;
 }
-function bonus($uid,$n){
-		$ch = curl_init();
-			curl_setopt($ch, CURLOPT_URL, "https://ai.caping.co.id/v2/event/random"); 
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-			$headers = array();
-					$headers[] = "Cookie: u=$uid;n=$n";
-					$headers[] = "User-Agent: Mozilla/5.0 (Linux; Android 6.0.1; Redmi 3S Build/MMB29M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/66.0.3359.126 Mobile Safari/537.36;CapingNews/4.1.4";
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-		$result = curl_exec($ch);
-	return $result;
-}
-
-
-
-
-
-
-
-
 
 
 
